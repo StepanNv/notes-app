506fa4892e56b40e0c6030f49603f2e79eedc4e8
StepanNv <nikolaevsdev@gmail.com>
2026-05-18 16:42:31 +0300

    fix(icons): update SVG icons to use currentColor for fill and remove unnecessary attributes for cleaner code

--- web/src/stores/useAuthStore.ts
+++ web/src/stores/useAuthStore.ts
@@ -6,6 +6,6 @@ type TUseAuthStore = {
 };
 
 export const useAuthStore = create<TUseAuthStore>()((set) => ({
-  isAuth: false,
+  isAuth: true,
   setIsAuth: (value: boolean) => set({ isAuth: value }),
 }));
