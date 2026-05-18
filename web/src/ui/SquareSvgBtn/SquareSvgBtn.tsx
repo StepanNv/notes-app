74717e672d7303889c825789316d001b8b017fa8
StepanNv <nikolaevsdev@gmail.com>
2026-05-18 18:39:00 +0300

    fix(SquareSvgBtn): change div to button element for improved accessibility and interaction

--- web/src/ui/SquareSvgBtn/SquareSvgBtn.tsx
+++ web/src/ui/SquareSvgBtn/SquareSvgBtn.tsx
@@ -1,6 +1,6 @@
 import styles from './SquareSvgBtn.module.scss';
 
 const SquareSvgBtn = ({ children }: { children: React.ReactNode }) => {
-  return <div className={styles.squareSvgBtn}>{children}</div>;
+  return <button className={styles.squareSvgBtn}>{children}</button>;
 };
 export default SquareSvgBtn;
