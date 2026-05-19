631601811b16ac3e208f776a2d2bd9063eb44c47
StepanNv <nikolaevsdev@gmail.com>
2026-05-19 10:43:06 +0300

    feat(Logo): introduce Logo component with styles and conditional app name display

---
+++ web/src/components/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+import styles from './Logo.module.scss';
+import { Link } from 'react-router-dom';
+import LogoIcon from '/src/assets/icons/logo.svg?react';
+
+const Logo = ({ isAppNameVisible }: { isAppNameVisible: boolean }) => {
+  return (
+    <Link className={styles.logo} to={'/'}>
+      <LogoIcon />
+      {isAppNameVisible && <div className={styles.appName}>My notes</div>}
+    </Link>
+  );
+};
+export default Logo;
