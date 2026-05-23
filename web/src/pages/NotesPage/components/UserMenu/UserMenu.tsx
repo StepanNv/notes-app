5788bd316acc1877f346460ac714eaf57954a259
StepanNv <nikolaevsdev@gmail.com>
2026-05-23 16:02:32 +0300

    refactor(UserMenu): update logout functionality to use accessToken from auth store and enable dropdown item interaction

--- web/src/pages/NotesPage/components/UserMenu/UserMenu.tsx
+++ web/src/pages/NotesPage/components/UserMenu/UserMenu.tsx
@@ -1,22 +1,23 @@
 import { useState, useRef } from 'react';
 import styles from './UserMenu.module.scss';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useClickOutside } from '../../../../hooks/useClickOutside';
+import { useAuthStore } from '../../../../stores/useAuthStore';
 
 const UserMenu = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const menuRef = useRef(null);
-  // const router = useNavigate();
+  const router = useNavigate();
+  const setAccessToken = useAuthStore((state) => state.setAccessToken);
 
   useClickOutside(menuRef, () => {
     if (isDropdownOpen) setDropdownOpen(false);
   });
 
-  // const logout = () => {
-  //   localStorage.setItem('auth', 'false');
-  //   setIsAuth(false);
-  //   router(`/`);
-  // };
+  const logout = () => {
+    setAccessToken(null);
+    router(`/`);
+  };
 
   return (
     <div className={styles.userMenu} ref={menuRef}>
@@ -29,7 +30,9 @@ const UserMenu = () => {
       {isDropdownOpen && (
         <div className={styles.dropdownMenu}>
           <div className={styles.dropdownItem}>Settings</div>
-          <div className={styles.dropdownItem}>Log out</div>
+          <div className={styles.dropdownItem} onClick={logout}>
+            Log out
+          </div>
         </div>
       )}
     </div>
