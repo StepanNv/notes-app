1b3dc7e7ffd390bf97c9be68c33d11685f8055ff
StepanNv <nikolaevsdev@gmail.com>
2026-05-23 09:52:06 +0300

    feat(main.tsx): integrate React Query by wrapping App component in QueryClientProvider for improved data fetching

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/main.scss';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={new QueryClient()}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
