c6c80eb0973009b2cccddeb9c705dbafb8786d21
StepanNv <nikolaevsdev@gmail.com>
2026-05-25 10:44:15 +0300

    feat(HttpClient): add securityWorker to include Authorization header with accessToken for authenticated requests

--- web/src/api/http-client.ts
+++ web/src/api/http-client.ts
@@ -1,6 +1,18 @@
+import { useAuthStore } from '../stores/useAuthStore';
 import { HttpClient } from './generated/http-client';
 
 export const httpClient = new HttpClient({
   baseURL: 'http://localhost:3000',
   // Другие параметры по необходимости
+  securityWorker: () => {
+    const accessToken = useAuthStore.getState().accessToken;
+
+    if (!accessToken) return;
+
+    return {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+  },
 });
