0be4aa88a89db2f373d15b2df2009ef0472135d5
StepanNv <nikolaevsdev@gmail.com>
2026-05-25 09:44:12 +0300

    feat(InfiniteNotesList): refactor notes state to use NoteDto and integrate NoteItem component for rendering notes

--- web/src/modules/InfiniteNotesList/components/InfiniteNotesList/InfiniteNotesList.tsx
+++ web/src/modules/InfiniteNotesList/components/InfiniteNotesList/InfiniteNotesList.tsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
 import styles from './InfiniteNotesList.module.scss';
 import NoNotesContent from '../NoNotesContent/NoNotesContent';
+import NoteItem from '../NoteItem/NoteItem';
+import type { NoteDto } from '../../../../api/generated/data-contracts';
 
 const InfiniteNotesList = () => {
-  const [notes, setNotes] = useState<string[]>([]);
+  const [notes, setNotes] = useState<Pick<NoteDto, 'id' | 'title' | 'text'>[]>(
+    [],
+  );
   return (
     <div className={styles.infiniteNotesList}>
       {notes.length > 0 ? (
         <div className={styles.notesList}>
           {notes.map((note) => (
-            <div key={note} className={styles.note}>
-              {note}
-            </div>
+            <NoteItem key={note.id} title={note.title} text={note.text} />
           ))}
         </div>
       ) : (
--- web/src/modules/InfiniteNotesList/components/NoteItem/NoteItem.tsx
+++ web/src/modules/InfiniteNotesList/components/NoteItem/NoteItem.tsx
@@ -1,15 +1,18 @@
 import styles from './NoteItem.module.scss';
+import DOMPurify from 'dompurify';
+import parse from 'html-react-parser';
+import type { NoteDto } from '../../../../api/generated/data-contracts';
 
-const NoteItem = () => {
+const NoteItem = ({ title, text }: Pick<NoteDto, 'title' | 'text'>) => {
   return (
     <div
       className={styles.noteItem}
       // onClick={openNote}
     >
       <div className={styles.noteContent}>
-        <div className={styles.title}>{/* {content.title} */}</div>
+        <div className={styles.title}>{title}</div>
         <div className={styles.textt}>
-          {/* {parse(DOMPurify.sanitize(content.mainText))} */}
+          {parse(DOMPurify.sanitize(text || ''))}
         </div>
       </div>
 
