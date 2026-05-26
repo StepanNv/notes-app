7c71db2fb1ca422368881d52d407ff9ab9abdc56
StepanNv <nikolaevsdev@gmail.com>
2026-05-26 18:33:19 +0300

    feat(Users): add UsersController for user information retrieval

---
+++ server/src/users/users.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { JwtAccessAuthGuard } from '../auth/guards/jwt-access-auth.guard';
+import type { TJwtPayload } from '../auth/types/jwt-payload';
+import { GetAccessTokenPayload } from '../auth/decorators/get-at-payload.decorator';
+import type { User } from '../../prisma/generated/client';
+import { ApiOperation } from '@nestjs/swagger';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @ApiOperation({ summary: 'Получить информацию о себе' })
+  @Get('/me')
+  @UseGuards(JwtAccessAuthGuard)
+  getMe(
+    @GetAccessTokenPayload() accessJwtPayload: TJwtPayload,
+  ): Promise<User | null> {
+    return this.usersService.getOne({ id: accessJwtPayload.userId });
+  }
+}
