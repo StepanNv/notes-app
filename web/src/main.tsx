02ee03c222ba4012c53ea8eae60bdefd5de50860
StepanNv <nikolaevsdev@gmail.com>
2026-05-29 07:37:07 +0300

    fix(root): prevent query client recreation on every render

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,9 +4,11 @@ import App from './App';
 import './styles/main.scss';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={new QueryClient()}>
+    <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
   </StrictMode>,
