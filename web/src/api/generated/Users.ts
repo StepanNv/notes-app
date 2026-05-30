49c471c4adbd0806db59785066ed8bed402c3b54
StepanNv <nikolaevsdev@gmail.com>
2026-05-30 13:40:03 +0300

    chore(api): regenerate api

---
+++ web/src/api/generated/Users.ts
@@ -0,0 +1,45 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-nocheck
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+import type { UsersControllerGetMeData } from "./data-contracts";
+import { HttpClient, type RequestParams } from "./http-client";
+
+export class Users<SecurityDataType = unknown> {
+  static readonly paths = {
+    usersControllerGetMe: `/users/me`,
+  } as const;
+
+  http: HttpClient<SecurityDataType>;
+
+  constructor(http: HttpClient<SecurityDataType>) {
+    this.http = http;
+  }
+
+  /**
+   * No description
+   *
+   * @tags Users
+   * @name UsersControllerGetMe
+   * @summary Получить информацию о себе
+   * @request GET:/users/me
+   * @secure
+   * @response `200` `UsersControllerGetMeData`
+   */
+  usersControllerGetMe = (params: RequestParams = {}) =>
+    this.http.request<UsersControllerGetMeData, any>({
+      path: `/users/me`,
+      method: "GET",
+      secure: true,
+      format: "json",
+      ...params,
+    });
+}
--- web/src/api/generated/data-contracts.ts
+++ web/src/api/generated/data-contracts.ts
@@ -10,6 +10,13 @@
  * ---------------------------------------------------------------
  */
 
+export interface GetMeResDto {
+  email: string;
+  username: string;
+  /** @format date-time */
+  createdAt: string;
+}
+
 export interface RegisterDto {
   /** @format email */
   email: string;
@@ -22,7 +29,7 @@ export interface RegisterDto {
   password: string;
 }
 
-export interface AuthDto {
+export interface AuthResDto {
   accessJwt: string;
 }
 
@@ -39,7 +46,7 @@ export interface AddNoteDto {
   text: string;
 }
 
-export interface AddNoteResponseDto {
+export interface AddNoteResDto {
   /** @format date-time */
   createdAt: string;
   /** @format date-time */
@@ -108,14 +115,12 @@ export interface UpdateNotesColorDto {
     | "TWELVEFTH";
 }
 
-export interface UpdatedNoteColorDto {
-  noteId: string;
-  /** @format date-time */
-  updatedAt: string;
-}
-
-export interface UpdateNotesColorResponseDto {
-  updatedNotesData: UpdatedNoteColorDto[];
+export interface UpdateNotesColorResDto {
+  updatedNotesData: {
+    noteId: string;
+    /** @format date-time */
+    updatedAt: string;
+  }[];
 }
 
 export interface UpdateNoteContentDto {
@@ -126,7 +131,7 @@ export interface UpdateNoteContentDto {
   updatedText: string;
 }
 
-export interface UpdateNoteContentResponseDto {
+export interface UpdateNoteContentResDto {
   /** @format date-time */
   updatedAt: string;
 }
@@ -140,20 +145,8 @@ export interface NoteDto {
   id: string;
   title: string | null;
   text: string | null;
-  colorKey:
-    | "FIRST"
-    | "SECOND"
-    | "THIRD"
-    | "FOURTH"
-    | "FIFTH"
-    | "SIXTH"
-    | "SEVENTH"
-    | "EIGHTH"
-    | "NINTH"
-    | "TENTH"
-    | "ELEVENTH"
-    | "TWELVEFTH";
-  status: "default" | "archived" | "trashed";
+  colorKey: object;
+  status: object;
   positionNumber: number;
   /** @format date-time */
   createdAt: string;
@@ -162,24 +155,22 @@ export interface NoteDto {
   authorId: string;
 }
 
-export interface GetNotesMetaDto {
+export interface GetNotesResDto {
+  notes: NoteDto[];
   next_last_id: string | null;
 }
 
-export interface GetNotesResponseDto {
-  data: NoteDto[];
-  meta: GetNotesMetaDto;
-}
+export type UsersControllerGetMeData = GetMeResDto;
 
-export type AuthControllerRegisterData = AuthDto;
+export type AuthControllerRegisterData = AuthResDto;
 
-export type AuthControllerLoginData = AuthDto;
+export type AuthControllerLoginData = AuthResDto;
 
 export type AuthControllerLogoutData = any;
 
-export type AuthControllerRefreshData = AuthDto;
+export type AuthControllerRefreshData = AuthResDto;
 
-export type NotesControllerAddNoteData = AddNoteResponseDto;
+export type NotesControllerAddNoteData = AddNoteResDto;
 
 export type NotesControllerArchiveNotesData = any;
 
@@ -191,9 +182,9 @@ export type NotesControllerRestoreTrashedNotesData = any;
 
 export type NotesControllerDeleteNotesData = any;
 
-export type NotesControllerUpdateNotesColorData = UpdateNotesColorResponseDto;
+export type NotesControllerUpdateNotesColorData = UpdateNotesColorResDto;
 
-export type NotesControllerUpdateNoteContentData = UpdateNoteContentResponseDto;
+export type NotesControllerUpdateNoteContentData = UpdateNoteContentResDto;
 
 export type NotesControllerUpdateNotePositionData = any;
 
@@ -212,4 +203,4 @@ export interface NotesControllerGetNotesParams {
   last_id?: string;
 }
 
-export type NotesControllerGetNotesData = GetNotesResponseDto;
+export type NotesControllerGetNotesData = GetNotesResDto;
