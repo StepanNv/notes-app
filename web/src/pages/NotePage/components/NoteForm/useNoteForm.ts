795f492ef3f5ff25c0cc5918930af91b0cf36fab
StepanNv <nikolaevsdev@gmail.com>
2026-06-05 20:36:45 +0300

    refactor(useNoteForm): enhance note submission logic with error handling and navigation after save

--- web/src/pages/NotePage/components/NoteForm/useNoteForm.ts
+++ web/src/pages/NotePage/components/NoteForm/useNoteForm.ts
@@ -3,16 +3,19 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import z from 'zod';
 import { addNote } from '../../api/add-note';
 import { updateContent } from '../../api/update-content';
+import { useNavigate } from 'react-router-dom';
+
+const formSchema = z.object({
+  title: z
+    .string()
+    .max(100, { message: 'Title cannot be longer than 100 characters' }),
+  text: z
+    .string()
+    .max(10000, { message: 'Text cannot be longer than 10000 characters' }),
+});
 
 export const useNoteForm = (noteId?: string) => {
-  const formSchema = z.object({
-    title: z
-      .string()
-      .max(100, { message: 'Title cannot be longer than 100 characters' }),
-    text: z
-      .string()
-      .max(10000, { message: 'Text cannot be longer than 10000 characters' }),
-  });
+  const navigate = useNavigate();
 
   const { register, handleSubmit, control } = useForm<
     z.infer<typeof formSchema>
@@ -21,17 +24,24 @@ export const useNoteForm = (noteId?: string) => {
     mode: 'onChange',
   });
 
-  const saveNote = handleSubmit((formData: z.infer<typeof formSchema>) => {
-    if (noteId) {
-      updateContent({
-        noteId,
-        updatedTitle: formData.title,
-        updatedText: formData.text,
-      });
-    } else {
-      addNote(formData);
+  const onSubmit = async (formData: z.infer<typeof formSchema>) => {
+    try {
+      if (noteId) {
+        await updateContent({
+          noteId,
+          updatedTitle: formData.title,
+          updatedText: formData.text,
+        });
+      } else {
+        await addNote(formData);
+      }
+      navigate('/notes');
+    } catch (error) {
+      console.error(error);
     }
-  });
+  };
+
+  const saveNote = handleSubmit(onSubmit);
 
   return { register, control, saveNote };
 };
