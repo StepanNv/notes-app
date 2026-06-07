a22dbc08e97347239e5d3d8aabedd4296616d5d5
StepanNv <nikolaevsdev@gmail.com>
2026-06-07 15:18:34 +0300

    feat(Logo): change logo container to button for navigation and add cursor pointer style

--- web/src/components/Logo/Logo.tsx
+++ web/src/components/Logo/Logo.tsx
@@ -1,12 +1,14 @@
 import styles from './Logo.module.scss';
 import LogoIcon from '/src/assets/icons/logo.svg?react';
+import { useNavigate } from 'react-router-dom';
 
 const Logo = ({ isAppNameVisible }: { isAppNameVisible: boolean }) => {
+  const navigate = useNavigate();
   return (
-    <div className={styles.logo}>
+    <button className={styles.logo} onClick={() => navigate('/')}>
       <LogoIcon />
       {isAppNameVisible && <div className={styles.appName}>Notes app</div>}
-    </div>
+    </button>
   );
 };
 export default Logo;
