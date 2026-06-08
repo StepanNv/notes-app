1efb1645fe4e628eafd439954d736dee3ae49e39
StepanNv <nikolaevsdev@gmail.com>
2026-06-08 18:08:54 +0300

    refactor(get-notes-res.dto): simplify structure by importing NoteDto and removing redundant properties

--- server/src/notes/dtos/res/get-notes-res.dto.ts
+++ server/src/notes/dtos/res/get-notes-res.dto.ts
@@ -1,31 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { Note } from '../../../../prisma/generated/client';
-import { colorKey, status } from '../../../../prisma/generated/enums';
-
-export class NoteDto implements Pick<
-  Note,
-  | 'id'
-  | 'title'
-  | 'text'
-  | 'colorKey'
-  | 'status'
-  | 'positionNumber'
-  | 'createdAt'
-  | 'updatedAt'
-  | 'authorId'
-> {
-  readonly id: string;
-  readonly title: string | null;
-  readonly text: string | null;
-  @ApiProperty({ enum: colorKey })
-  readonly colorKey: colorKey;
-  @ApiProperty({ enum: status })
-  readonly status: status;
-  readonly positionNumber: number;
-  readonly createdAt: Date;
-  readonly updatedAt: Date;
-  readonly authorId: string;
-}
+import { NoteDto } from "../note.dto";
 
 export class GetNotesResDto {
   readonly notes: NoteDto[];
