8475b1cc5aedc00b341e10f5c106f582318ddf98
StepanNv <nikolaevsdev@gmail.com>
2026-06-16 14:34:13 +0300

    feat(EditorHeader): add DownNoteBtn and UpNoteBtn components for note navigation

--- web/src/modules/EditorHeader/components/DownNoteBtn/DownNoteBtn.tsx
+++ web/src/modules/EditorHeader/components/DownNoteBtn/DownNoteBtn.tsx
@@ -1,11 +1,11 @@
 import SquareSvgBtn from '../../../../ui/SquareSvgBtn/SquareSvgBtn';
 import ArrowDownIcon from '/src/assets/icons/arrow-down.svg?react';
 
-const ArrowDownBtn = ({ onClick }: { onClick: () => void }) => {
+const DownNoteBtn = ({ onClick }: { onClick: () => void }) => {
   return (
     <SquareSvgBtn onClick={onClick}>
       <ArrowDownIcon />
     </SquareSvgBtn>
   );
 };
-export default ArrowDownBtn;
+export default DownNoteBtn;
