9cc8af3fd2dcc384a523a3b5fbbf5e9f2a82a6f3
StepanNv <nikolaevsdev@gmail.com>
2026-06-16 13:16:52 +0300

    feat(App): add ErrorAlertsBox component to App for centralized error handling

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './router/AppRouter';
 import './styles/main.scss';
+import { ErrorAlersBox } from './modules/ErrorAlertsBox';
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <AppRouter />
+        <ErrorAlersBox />
       </BrowserRouter>
     </div>
   );
