ee899d4258ada1a995962cb6a77cf6129eea773b
StepanNv <nikolaevsdev@gmail.com>
2026-06-22 15:44:15 +0300

    chore(notes): remove unused DTO files for archiving and trashing notes

--- server/src/notes/dtos/req/unarchive-notes.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ArrayNotEmpty, ArrayUnique, IsArray, IsString } from 'class-validator';
-
-export class UnarchiveNotesDto {
-  @IsArray()
-  @ArrayNotEmpty()
-  @ArrayUnique()
-  @IsString({ each: true })
-  readonly noteIds: string[];
-}
