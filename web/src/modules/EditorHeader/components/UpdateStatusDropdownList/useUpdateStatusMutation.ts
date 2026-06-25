f1232f1295dc180ead17cef631689b29eff2ef36
StepanNv <nikolaevsdev@gmail.com>
2026-06-25 15:01:47 +0300

    refactor(UpdateStatusMutation): clear selected notes on successful status update to enhance user experience

--- web/src/modules/EditorHeader/components/UpdateStatusDropdownList/useUpdateStatusMutation.ts
+++ web/src/modules/EditorHeader/components/UpdateStatusDropdownList/useUpdateStatusMutation.ts
@@ -4,15 +4,18 @@ import type { UpdateStatusDto } from '../../../../api/generated/data-contracts';
 import { useErrorsStore } from '../../../../modules/ErrorAlertsBox/stores/useErrorsStore';
 import { updateNoteStatus } from '../../api/update-note-status';
 import { useQueryClient } from '@tanstack/react-query';
+import { useNotesSelectionStore } from '../../../../stores/useNotesSelectionStore';
 
 export const useUpdateStatusMutation = () => {
   const queryClient = useQueryClient();
   const addError = useErrorsStore((state) => state.addError);
+  const clearSelectedNotes = useNotesSelectionStore((state) => state.clear);
 
   return useMutation({
     mutationFn: (data: UpdateStatusDto) => updateNoteStatus(data),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['notes'] });
+      clearSelectedNotes();
     },
     onError: (error: AxiosError<{ message: string }>) => {
       const message = error.response?.data?.message;
