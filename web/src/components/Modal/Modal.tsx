8fbf9d07080f141f4b0bf6e88ba811c84e1b2b21
StepanNv <nikolaevsdev@gmail.com>
2026-06-29 19:37:00 +0300

    feat(Modal): add click outside functionality to close modal when clicking outside of it

--- web/src/components/Modal/Modal.tsx
+++ web/src/components/Modal/Modal.tsx
@@ -1,6 +1,8 @@
 import Backdrop from '../../ui/Backdrop/Backdrop';
 import styles from './Modal.module.scss';
 import { useModalStore } from '../../stores/useModalStore';
+import { useClickOutside } from '../../hooks/useClickOutside';
+import { useRef } from 'react';
 
 type TModalProps = {
   children: React.ReactNode;
@@ -9,9 +11,13 @@ type TModalProps = {
 
 const Modal = ({ children, isOpen }: TModalProps) => {
   const { closeModal } = useModalStore();
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useClickOutside(modalRef, () => closeModal());
+
   return (
-    <Backdrop isOpen={isOpen} onClose={closeModal}>
-      <div className={styles.modal}>{children}</div>
+    <Backdrop isOpen={isOpen}>
+      <div className={styles.modal} ref={modalRef}>{children}</div>
     </Backdrop>
   );
 };
