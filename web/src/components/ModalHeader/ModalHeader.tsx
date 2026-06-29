3f23bd8e295285a69be02b02758a75e71299df88
StepanNv <nikolaevsdev@gmail.com>
2026-06-29 17:04:17 +0300

    refactor(ModalHeader): integrate modal store for closing functionality and simplify props structure

--- web/src/components/ModalHeader/ModalHeader.tsx
+++ web/src/components/ModalHeader/ModalHeader.tsx
@@ -1,17 +1,13 @@
 import styles from './ModalHeader.module.scss';
 import CrossBtn from '../CrossBtn/CrossBtn';
+import { useModalStore } from '../../stores/useModalStore';
 
-const ModalHeader = ({
-  title,
-  onModalClose,
-}: {
-  title: string;
-  onModalClose: () => void;
-}) => {
+const ModalHeader = ({ title }: { title: string }) => {
+  const { closeModal } = useModalStore();
   return (
     <div className={styles.modalHeader}>
       <h2 className={styles.modalHeaderTitle}>{title}</h2>
-      <CrossBtn onClick={onModalClose} />
+      <CrossBtn onClick={closeModal} />
     </div>
   );
 };
