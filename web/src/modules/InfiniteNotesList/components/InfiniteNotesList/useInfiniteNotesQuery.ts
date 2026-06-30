6c492f773b6ee55b890879b252d89bc20062f107
StepanNv <nikolaevsdev@gmail.com>
2026-06-30 18:08:56 +0300

    refactor(useInfiniteNotesQuery): integrate sorting from store and enhance query key structure

--- web/src/modules/InfiniteNotesList/components/InfiniteNotesList/useInfiniteNotesQuery.ts
+++ web/src/modules/InfiniteNotesList/components/InfiniteNotesList/useInfiniteNotesQuery.ts
@@ -1,15 +1,20 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { getNotes } from '../../api/get-notes';
 import type { NotesControllerGetManyParams } from '../../../../api/generated/data-contracts';
+import { useNotesSortStore } from '../../../../stores/useNotesSortStore';
 
 export const useInfiniteNotesQuery = (query: NotesControllerGetManyParams) => {
+  const sort = useNotesSortStore((state) => state.sort);
   return useInfiniteQuery({
-    queryKey: ['notes'],
+    queryKey: [
+      'notes',
+      { status: query.status, sort: sort, limit: query.limit },
+    ],
     queryFn: async ({ pageParam }) => {
       // pageParam - информирует о том с какой заметки начинать загружать данные
       const res = await getNotes({
         status: query.status,
-        sort: query.sort,
+        sort: sort,
         limit: query.limit,
         ...(pageParam ? { last_id: pageParam } : {}),
       });
@@ -24,5 +29,6 @@ export const useInfiniteNotesQuery = (query: NotesControllerGetManyParams) => {
 
       return lastPage.next_last_id;
     },
+    refetchOnWindowFocus: false,
   });
 };
