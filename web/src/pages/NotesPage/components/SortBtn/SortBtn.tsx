fbe0ea575901c8ad8ebb24382c53ba5c6d97a1c9
StepanNv <nikolaevsdev@gmail.com>
2026-06-30 12:23:55 +0300

    fix(SortBtn): prevent event propagation on button click to enhance functionality

--- web/src/pages/NotesPage/components/SortBtn/SortBtn.tsx
+++ web/src/pages/NotesPage/components/SortBtn/SortBtn.tsx
@@ -3,7 +3,13 @@ import SortIcon from '/src/assets/icons/sort-icon.svg?react';
 
 const SortBtn = ({ onClick }: { onClick: () => void }) => {
   return (
-    <button className={styles.sortBtn} onClick={onClick}>
+    <button
+      className={styles.sortBtn}
+      onClick={(e) => {
+        e.stopPropagation();
+        onClick();
+      }}
+    >
       <SortIcon />
     </button>
   );
