4d59a654da51fcb43e260633ef80fda06f1661be
StepanNv <nikolaevsdev@gmail.com>
2026-07-12 17:01:02 +0300

    refactor(SignUpForm): remove error handling logic and unused imports to streamline sign-up process

--- web/src/pages/AuthPage/components/SignUpForm/useSignUpForm.ts
+++ web/src/pages/AuthPage/components/SignUpForm/useSignUpForm.ts
@@ -2,8 +2,6 @@ import { useForm } from 'react-hook-form';
 import type { SignUpDto } from '../../../../api/generated/data-contracts';
 import { useSignUpMutation } from './useSignUpMutation';
 import { useNavigate } from 'react-router-dom';
-import type { AxiosError } from 'axios';
-import { useErrorsStore } from '../../../../modules/ErrorAlertsBox';
 import { useConfirmationEmailStore } from '../../stores/useConfirmationEmailStore';
 
 export const useSignUpForm = () => {
@@ -16,7 +14,6 @@ export const useSignUpForm = () => {
     (state) => state.setTrueEnteredPassword,
   );
   const navigate = useNavigate();
-  const addError = useErrorsStore((state) => state.addError);
 
   const submit = handleSubmit((formData) => {
     signUpMutation.mutate(formData, {
@@ -24,14 +21,6 @@ export const useSignUpForm = () => {
         setConfirmationEmail(formData.email);
         setTrueEnteredPassword(formData.password);
         navigate('/sign-in/confirm-code');
-      },
-      onError: (error: AxiosError<{ message: string }>) => {
-        const message = error.response?.data?.message;
-        if (message) {
-          addError(message);
-        } else {
-          addError('Something went wrong. Please try again later.');
-        }
       },
     });
   });
