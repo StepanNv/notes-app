71d1da5f31fedb089c010e27173ee47ae4e1bb7f
StepanNv <nikolaevsdev@gmail.com>
2026-07-17 21:03:16 +0300

    refactor(LandingContent): update import path for styles to match component naming convention

--- web/src/pages/LandingPage/components/LandingContent/LandingContent.tsx
+++ web/src/pages/LandingPage/components/LandingContent/LandingContent.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import styles from './LandingPageMain.module.scss';
+import styles from './LandingContent.module.scss';
 
 const LandingContent = () => {
   return (
