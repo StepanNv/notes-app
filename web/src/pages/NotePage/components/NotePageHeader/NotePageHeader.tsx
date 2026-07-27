e0422d5a3c73fba7e9cd9d8824c9c49d319d7c43
StepanNv <nikolaevsdev@gmail.com>
2026-07-27 21:44:16 +0300

    refactor(NotePageHeader): remove unused Link import to streamline component

--- web/src/pages/NotePage/components/NotePageHeader/NotePageHeader.tsx
+++ web/src/pages/NotePage/components/NotePageHeader/NotePageHeader.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Header from '../../../../ui/Header/Header';
 import styles from './NotePageHeader.module.scss';
 import BlueHeaderBtn from '../../../../ui/BlueHeaderBtn/BlueHeaderBtn';
