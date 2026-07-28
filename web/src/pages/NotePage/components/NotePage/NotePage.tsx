bbd133e5329b36dfe0f2595a2f4fd6a018c105f0
StepanNv <nikolaevsdev@gmail.com>
2026-07-28 21:02:19 +0300

    feat(NotePage): add changing text color while light or dark note color

--- web/src/pages/NotePage/components/NotePage/NotePage.tsx
+++ web/src/pages/NotePage/components/NotePage/NotePage.tsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 import { useGetNote } from '../../hooks/useGetNote';
 import { NOTE_COLORS } from '../../../../consts/noteColors';
 import { useAppSettingsStore } from '../../../../stores/useAppSettingsStore';
+import type { NoteDto } from '../../../../api/generated/data-contracts';
 
 const contentTranlations = {
   en: {
@@ -17,7 +18,19 @@ const contentTranlations = {
   },
 };
 
-const NotePage = () => {
+const isNoteColorLight = (
+  noteData: NoteDto | undefined,
+  theme: 'light' | 'dark',
+  isNew: boolean,
+) => {
+  if (theme === 'light') {
+    if (isNew || noteData?.colorKey === 'FIRST') return true;
+  } else {
+    return false;
+  }
+};
+
+const NotePage = ({ isNew }: { isNew: boolean }) => {
   const { id } = useParams();
   const { data, isLoading, isError } = useGetNote(id);
   const language = useAppSettingsStore((state) => state.language);
@@ -25,39 +38,20 @@ const NotePage = () => {
     language === 'en' ? contentTranlations.en : contentTranlations.ru;
   const theme = useAppSettingsStore((state) => state.theme);
 
-  if (isLoading) {
-    return (
-      <div className={styles.notePage}>
-        <NotePageHeader />
-        <main className={styles.main}>
-          <div className={styles.content}>{content.loadingText}</div>
-        </main>
-      </div>
-    );
-  }
-
-  if (isError || !data) {
-    return (
-      <>
-        <NotePageHeader />
-        <main className={styles.main}>
-          <div className={styles.content}>{content.errorText}</div>
-        </main>
-      </>
-    );
-  }
-
   return (
     <div
       className={styles.notePage}
       style={{
-        backgroundColor: NOTE_COLORS[data.note.colorKey],
-        color:
-          theme === 'light' && data.note.colorKey !== 'FIRST' ? 'white' : '',
+        backgroundColor: data && NOTE_COLORS[data.note.colorKey],
+        color: isNoteColorLight(data?.note, theme, isNew) ? '' : 'white',
       }}
     >
       <NotePageHeader />
       <main className={styles.main}>
+        {isLoading && (
+          <div className={styles.content}>{content.loadingText}</div>
+        )}
+        {isError && <div className={styles.content}>{content.errorText}</div>}
         <NoteForm />
       </main>
     </div>
